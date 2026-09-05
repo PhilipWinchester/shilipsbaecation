149e31a435cdb4770dd11d46ead1411a0ff315f3
fplbuddy <fplhelper7@gmail.com>
2026-09-05 22:50:39 +0100

    Update navigation links and add new destination pages for Bruges, Shuzenji, South Downs, Wensleydale, and Ystad

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ function confirmBooking(destination, redirectUrl, button) {
       button.disabled = true;
       button.textContent = 'Sending...';
     }
-    var websiteLink = window.location.href.replace(/[^/]*$/, 'front.html');
+    var websiteLink = window.location.origin + '/trip/';
     emailjs.send(EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, {
       destination: destination,
       date_from: from,
